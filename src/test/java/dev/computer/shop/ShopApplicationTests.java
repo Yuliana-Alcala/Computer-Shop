a0ad39cd6493bf71b879f5c23053a3fd838b36f0
Yuliana-Alcala <alcalabyulianac@gmail.com>
2024-12-16 16:36:41 +0100

    first commit

---
+++ src/test/java/dev/computer/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.computer.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
